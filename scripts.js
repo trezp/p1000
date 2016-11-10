7d77e1d5e5350ebcf828a1f0735d7e2346ba1b94
Treasure Porth <treasure.rose@gmail.com>
2016-11-09 16:43:30 -0800

    added comments

--- scripts.js
+++ scripts.js
@@ -1,52 +1,60 @@
+
 (function(){
   var pageHeader   = document.getElementById('page-header'),
-      search       = `<div class="student-search">
-                    <input id="student-search" placeholder="Search for students...">
-                    </div>`,
       studentArray = document.getElementsByClassName('student-item'),
       page         = document.getElementById('page'),
-      pageButtons  = '<div class="pagination"><ul id="page-list"></ul></div>';
+      err          = document.createElement('li'),
+      pageButtons  = '<div class="pagination"><ul id="page-list"></ul></div>',
+      search       = `<div class="student-search">
+                    <input id="student-search" placeholder="Search for students...">
+                    </div>`;
 
-
-
-  pageHeader.innerHTML = search;
+//put pagination and search on page and create error message
+  pageHeader.innerHTML += search;
   page.innerHTML      += pageButtons;
-  var err = document.createElement('li');
-  err.innerHTML = "Oops! There are no matching students.";
+  err.innerHTML = "<span class='err-message'>Oops! There are no matching students.</span>";
 
   function peopleSearch(){
     var searchValue, studentName, searchList = [];
 
+    searchValue  = document.getElementById('student-search').value;
+
+  /* hides all students, gets name and email from each item; pushes student to new array if match,
+  shows students who match search */
     for(var i = 0; i < studentArray.length; i++){
       studentArray[i].style.display = 'none';
-      searchValue  = document.getElementById('student-search').value,
-      studentName = studentArray[i].firstElementChild.children[1].textContent;
-
+      studentName = studentArray[i].firstElementChild.children[1].textContent,
+      studentEmail = studentArray[i].firstElementChild.children[2].textContent;
 
       if(studentName.indexOf(searchValue) > -1){
         searchList.push(studentArray[i]);
-        studentArray[i].style.display = "block";
       }
     }
 
+// if search is more than three characters and no match, display error
     if(searchValue.length >= 3 && searchList.length === 0){
       page.appendChild(err);
     } else {
       err.remove();
     }
 
+//hides all but first ten matched students
     searchList.forEach(function(student, index){
-      if(index > 9){
-        student.style.display = "none";
+      if(index < 9){
+        student.style.display = "block";
       }
     });
 
+/* sends matched list to pagination function,
+adds/removes correct event listener to pagination buttons,
+returns list of matched students */
     makePaginationButtons(searchList);
     document.removeEventListener('click', onLoadEventListener);
     document.addEventListener('click', searchEventListener);
     return searchList;
   }
 
+//hides all but first 10 students, sends all students to pagination function
   function firstTenStudentsOnLoad(){
     for(var i = 0; i < studentArray.length; i++){
       if(i > 9){
@@ -55,12 +63,13 @@
         studentArray[i].style.display = 'block';
       }
     }
-    makePaginationButtons(studentArray);
 
+    makePaginationButtons(studentArray);
     document.removeEventListener('click', searchEventListener);
     document.addEventListener('click', onLoadEventListener);
   }
 
+//clears any previous pagination, student in a list of students and adds pagination buttons
   function makePaginationButtons(list){
     var numToPage  = list.length,
         numPerPage = 10,
@@ -79,6 +88,8 @@
     }
   }
 
+/* takes in an array of students and an int
+based on which page button is clicked, shows and hides correct students */
   function paginate(link, list){
     var start = (link * 10) - 10,
         end   = (link * 10) - 1;
@@ -92,18 +103,21 @@
     }
   }
 
+/* event listeners for initial page load and search functionality.
+if the clicked item is a link, gets link number and feeds to paginate function */
   function genericEventListener(e, list){
     e.preventDefault();
     if(e.target.tagName.toLowerCase() === "a"){
-      var currentList = list,
-          link = e.target.innerText;
+      link = e.target.innerText;
       paginate(link, list);
     }
   }
-
+//takes in list of matched students returned by search
   function searchEventListener(e){
     genericEventListener(e, peopleSearch());
   }
+
+//takes in full list of students
   function onLoadEventListener(e){
     genericEventListener(e, studentArray);
   }
